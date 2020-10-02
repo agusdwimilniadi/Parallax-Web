8ddc09c9068d3bd4db4f16973e4fe99db1cce086
agusdwimilniadi <agusdwimill@gmail.com>
2020-10-02 13:52:05 +0700

    Menambahkan animasi di jumbotron

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-$('.page-scroll').on('click', function(){
+$('.page-scroll').on('click', function(e){
 
     //ambil isi href
     var tujuan = $(this).attr('href');
     //tangkap elemen
     var elemenTujuan = $(tujuan);
-    console.log(elemenTujuan.offset().top);
 
+    $('body').animate({
+        scrollTop: elemenTujuan.offset().top -50
+    }, 1250, 'easeInOutExpo');
 
+    e.preventDefault();
 });
+
+
+
+
+
+// menyimpan parallax
+
+$(window).scroll(function(){
+    var wSroll = $(this).scrollTop();
+    
+    
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wSroll/4 +'%)'
+    })
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wSroll/2 +'%)'
+    })
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wSroll/1.2 +'%)'
+    })
+
+    // PORTFOLIO
+})
